e43d6aec642398a1b6b3f0ffa835135918903846
youngwind <540506594@qq.com>
2016-04-17 14:16:12 +0800

    使用混合模式改写

--- thief.js
+++ thief.js
@@ -1,20 +1,19 @@
 (function () {
 
-  // 构造模式
-  var T = function (selector) {
+  // 混合模式 = 构造模式 + 原型模式
 
-    // 获取DOM元素
+  // 构造模式定义属性
+  function T(selector) {
     this.dom = document.querySelector(selector);
+  }
 
-    // 添加类
-    this.addClass = function (className) {
-      this.dom.classList.add(className);
-    };
+  // 原型模式定义方法
+  T.prototype.addClass = function (className) {
+    this.dom.classList.add(className)
+  };
 
-    // 删除类
-    this.removeClass = function (className) {
-      this.dom.classList.remove(className);
-    };
+  T.prototype.removeClass = function (className) {
+    this.dom.classList.remove(className)
   };
 
   window.T = T;
